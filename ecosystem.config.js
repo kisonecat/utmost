5d606b09a4f9c6bde0c4f472eb0349072b435b81
Jim Fowler <fowler@math.osu.edu>
2017-01-16 16:52:34 -0500

    setting port

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,15 +1,17 @@
 module.exports = {
-  apps : [{
-    name        : "app",
-    script      : "./app.js",
-    instances   : "1",
-    exec_mode   : "cluster",
-    watch       : true,
-    env: {
-      "NODE_ENV": "development",
-    },
-    env_production : {
-       "NODE_ENV": "production"
-    }
-  }]
+    apps : [{
+	name        : "app",
+	script      : "./app.js",
+	instances   : "1",
+	exec_mode   : "cluster",
+	watch       : true,
+	env: {
+	    "NODE_ENV": "development",
+	    "PORT": "7000"	
+	},
+	env_production : {
+	    "NODE_ENV": "production",
+	    "PORT": "7000"
+	}
+    }]
 }
